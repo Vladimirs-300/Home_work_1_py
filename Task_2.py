9a9a11a382cc70b0613a813817fdcf8838c64dde
vladimir <slr-klesks-300@yandex.ru>
2022-12-09 22:22:59 +1000

    исправление

--- Task_2.py
+++ Task_2.py
@@ -1,12 +1,12 @@
 # ЗАдача 2. 
 # Напишите программу для проверки ложности утверждения
 # (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
-
+print ("a y z w")
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if not (x or y or z or w) == (not x and not y and not z and not w):
+                if not ((w and z) or not y or (not x == (not w))):
                     print(x, y, z, w)
 
 
